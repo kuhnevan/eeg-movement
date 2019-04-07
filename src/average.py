fixed problem with average.py

src/average.py
@@ -9,11 +9,11 @@ from scipy.io import savemat
 import numpy as np
 
 for j in range(1, 53):
-    left = "../../processed-data/s{}-left.mat".format(j)
-    right = "../../processed-data/s{}-right.mat".format(j)
-
-    leftMat = np.average(loadmat(left), axis=0)
-    rightMat = np.average(loadmat(right), axis=0)
+    left = loadmat("../../processed-data/s{}-left.mat".format(j))
+    right = loadmat("../../processed-data/s{}-right.mat".format(j))
+    
+    leftMat = np.average(left.movement_left, axis=0)
+    rightMat = np.average(right.movement_right, axis=0)
     
     savemat('../../processed-data/s{}-left-image.mat'.format(j),
             {'movement_left': leftMat})