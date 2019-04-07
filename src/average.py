7d8fc934dfacce2bf3bc647414e2e44bb9378f14
Evan Kuhn <kuhnep@miamioh.edu>
2019-04-07 14:25:56 -0400

    Fixed dictionary access bug

--- src/average.py
+++ src/average.py
@@ -9,11 +9,12 @@ from scipy.io import savemat
 import numpy as np
 
 for j in range(1, 53):
+    # Load in the dictionary containing our array of data
     left = loadmat("../../processed-data/s{}-left.mat".format(j))
     right = loadmat("../../processed-data/s{}-right.mat".format(j))
     
-    leftMat = np.average(left.movement_left, axis=0)
-    rightMat = np.average(right.movement_right, axis=0)
+    leftMat = np.average(left['movement_left'], axis=0)
+    rightMat = np.average(right['movement_right'], axis=0)
     
     savemat('../../processed-data/s{}-left-image.mat'.format(j),
             {'movement_left': leftMat})
