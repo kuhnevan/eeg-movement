be1ce185ef50b4ff9e36fce36ab76c7d8da16b87
moormabt <moormabt@miamioh.edu>
2019-04-12 16:21:52 -0400

    added classification accuracy

--- src/sLDA.py
+++ src/sLDA.py
@@ -24,7 +24,7 @@ y = np.concatenate((y, yfiller), axis=None)
 
 
 
-for j in range(2, 53):
+for j in range(2, 37):
     left = "../../processed-data-image/s{}-left-image.mat".format(j)
     right = "../../processed-data-image/s{}-right-image.mat".format(j)
     leftMat = loadmat(left)["movement_left"]
@@ -43,6 +43,34 @@ for j in range(2, 53):
 clf = LinearDiscriminantAnalysis(solver='lsqr', shrinkage='auto').fit(X, y)
 
 #test prediction with testing
-testImage = "../../processed-data-image/s1-left-image.mat"
-testImageMat = loadmat(testImage)["movement_left"]
-print(clf.predict(testImageMat))
+#testImage = "../desktop/448/processed-data-image/s1-left-image.mat"
+#testImageMat = loadmat(testImage)["movement_left"]
+#print(clf.predict(testImageMat))
+
+numTested = 0;
+numRight = 0;
+numRH = 0;
+numLH = 0
+
+for k in range(37, 53):
+    testImage = "../../processed-data-image/s{}-left-image.mat".format(k)
+    testImageMat = loadmat(testImage)["movement_left"]
+    resultMat = clf.predict(testImageMat)
+    for aResult in resultMat:
+        numTested = numTested + 1
+        numLH = numLH + 1
+        if aResult==1:
+            numRight = numRight + 1
+    
+    testImage = "../../processed-data-image/s{}-right-image.mat".format(k)
+    testImageMat = loadmat(testImage)["movement_right"]
+    resultMat = clf.predict(testImageMat)
+    for aResult in resultMat:
+        numTested = numTested + 1
+        numRH = numRH + 1
+        if aResult==2:
+            numRight = numRight + 1
+            
+print(numRight/numTested)
+print(numRH)
+print(numLH)
