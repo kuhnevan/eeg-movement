dd4cfc64985dd52d21ff3ebbdb11ea605510c03b
Evan Kuhn <kuhnep@miamioh.edu>
2019-04-23 10:56:05 -0400

    Imported modules into one script. Renamed script files

--- modules/average.py
+++ modules/average.py
@@ -8,15 +8,16 @@ from scipy.io import loadmat
 from scipy.io import savemat
 import numpy as np
 
-for j in range(1, 53):
-    # Load in the dictionary containing our array of data
-    left = loadmat("../../processed-data/s{}-left.mat".format(j))
-    right = loadmat("../../processed-data/s{}-right.mat".format(j))
-    
-    leftMat = np.average(left['movement_left'], axis=2)
-    rightMat = np.average(right['movement_right'], axis=2)
-    
-    savemat('../../processed-data/s{}-left-image.mat'.format(j),
-            {'movement_left': leftMat})
-    savemat('../../processed-data/s{}-right-image.mat'.format(j),
-            {'movement_right': rightMat})
+def averageEpochs():
+    for j in range(1, 53):
+        # Load in the dictionary containing our array of data
+        left = loadmat("../../processed-data/s{}-left.mat".format(j))
+        right = loadmat("../../processed-data/s{}-right.mat".format(j))
+        
+        leftMat = np.average(left['movement_left'], axis=2)
+        rightMat = np.average(right['movement_right'], axis=2)
+        
+        savemat('../../processed-data/s{}-left-image.mat'.format(j),
+                {'movement_left': leftMat})
+        savemat('../../processed-data/s{}-right-image.mat'.format(j),
+                {'movement_right': rightMat})
---
+++ modules/pipeline.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 19 16:23:08 2019
+
+@author: PoonTyphoon
+"""
+
+import preprocess
+import average
+import sLDA
+
+# 
+
+
+sLDA.runSLDA()
+
+
+
+
+
